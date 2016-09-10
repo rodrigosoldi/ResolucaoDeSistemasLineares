5df1785c289d84e072bf32196a127b8f19380d2b
emannuelOC <emannuel.carvalho@outlook.com>
2016-09-10 01:18:55 -0300

    Correção do algoritmo de pivotamento

--- pivotamento.py
+++ pivotamento.py
@@ -6,6 +6,11 @@
 para i de 0 até N - 1
     para j de i + 1 até N
         n1 recebe Aji
+        se n1 é igual a 0
+            para k de i até N
+                se A[k][i] é diferente de 0
+                    swap(A, i, k)
+                    n1 recebe A[j][i]
         pivot recebe Aii
         div recebe pivot / n1
         Aj recebe Aj - Ai*div
@@ -32,6 +37,11 @@ def pivotamento(A):
     for i in xrange(len(A) - 1):
         for j in xrange(i + 1,len(A)):
             n1 = A[j][i]
+            if n1 == 0:
+                for k in xrange(i,len(A)):
+                    if A[k][i] != 0:
+                        swap(A, i, k)
+                        n1 = A[j][i]
             n2 = A[i][i]
             if n2 == 0:
                 continue
@@ -52,6 +62,15 @@ def sumVector(v1, v2):
         newV += [v1[i] + v2[i]]
     return newV
 
+def swap(A, i, j):
+    newI = []
+    newJ = []
+    for index in xrange(len(A) + 1):
+        newI += [A[j][index]]
+        newJ += [A[i][index]]
+    A[i] = newI
+    A[j] = newJ
+
 def printMatrix(A):
     for i in A:
         print "|\t",
@@ -59,7 +78,7 @@ def printMatrix(A):
             print "%.1f" %(j), "\t",
         print "\t|"
 
-A = [[1.0, -1.0, 2.0, -1.0, -8.0], [1.0, 1.0, 1.0, 0.0, -2.0], [2.0, -2.0, 3.0, -3.0, -20], [1.0, -1.0, 4.0, -3.0, 4.0]]
+A = [[1.0, -1.0, 2.0, -1.0, -8.0], [2.0, -2.0, 3.0, -3.0, -20], [1.0, 1.0, 1.0, 0.0, -2.0], [1.0, -1.0, 4.0, -3.0, 4.0]]
 printMatrix(A)
 print("\n\n")
 printMatrix(pivotamento(A))
